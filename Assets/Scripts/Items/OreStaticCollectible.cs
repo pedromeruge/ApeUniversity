b3c076fe85d7c6cc4b7b8fa47a3dd06349ec7fd9
pedromeruge <pedromeruge@gmail.com>
2025-03-08 00:11:08 +0000

    More ore types, and started particles on collect gold

--- Assets/Scripts/Items/OreStaticCollectible.cs
+++ Assets/Scripts/Items/OreStaticCollectible.cs
@@ -3,14 +3,27 @@ using UnityEngine;
 public class OreStaticCollectible : MonoBehaviour, IStaticCollectible
 {
     [SerializeField] private int oreValue = 200;
+    [SerializeField] private ParticleSystem moneyParticles;
+    [SerializeField] private SpriteRenderer oreSprite;
+    private bool once = false;
     public void onCollect(PlayerStats playerStats) {
+        if (once) return; // if already collected, but still playing the particle animation, dont count it again, just ignore it
+        once = true;
         playerStats.modifyMoney(oreValue);
-        playMoneyParticles();
-        Destroy(this.gameObject);
+        destroyObj();
     }
 
-    private void playMoneyParticles() {
-        // TODO: add particles
+    private void destroyObj() {
+        var particlesDuration = moneyParticles.main.duration;
+
+        moneyParticles.Play();
+        oreSprite.enabled = false; // Hide the ore sprite, so it appears it has been collected
+        this.Invoke(nameof(destroyParticles), particlesDuration);
     }
+
+    // Destroy the whole gameobject after particles have finished playing
+    void destroyParticles() {
+        Destroy(this.gameObject);
+    }  
 }
 
