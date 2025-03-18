536ce5243570db4af413e55dd72a50a8d567055a
Aecesaje <martimmaci02@gmail.com>
2025-03-18 21:56:41 +0000

    é o bicho

--- Assets/Scripts/Enemies/BatDamageScript.cs
+++ Assets/Scripts/Enemies/BatDamageScript.cs
@@ -1,14 +1,29 @@
 using UnityEngine;
 
-public class BatDamageScript : BaseDamageable
+public class BatDamageScript : MonoBehaviour
 {
-    //[SerializeField] private MonkeyCatchableScript monkeyCatchableScript;
-    public PlayerStats playerStats;
+    [SerializeField] private int damage = 1;
+    [SerializeField] private float collisionCooldown = 1.0f;
 
-    public override void TakeDamage(int damage = -1)
+    private float currentCollisionCooldown = 0.0f;
+
+    void OnTriggerEnter2D(Collider2D collider)
     {
-        bool isDead = playerStats.modifyLives(damage);
-        CallDamageFlash(); // blink effect
-        
+        if(currentCollisionCooldown <= 0.0f){
+            Vector3 objPos = transform.position;
+            CallInterfaces.triggerExplosive(collider, objPos);
+            CallInterfaces.hitDamageable(collider, objPos, damage);
+            //CallInterfaces.signalDestructibles(collider, objPos, 0.01f);
+            Debug.Log("Colliding with: "+ collider.name);
+            currentCollisionCooldown = collisionCooldown;
+        }
+        //Destroy(gameObject); // destroy arrow on collision, regardless if wall or damageable entity
+    }
+
+    void Update()
+    {
+        if(currentCollisionCooldown > 0.0f){
+            currentCollisionCooldown -= Time.deltaTime;
+        }
     }
 }
